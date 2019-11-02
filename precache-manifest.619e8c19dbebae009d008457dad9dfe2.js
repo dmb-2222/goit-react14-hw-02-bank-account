3abf522d1b271a45c9dcd9b3953f5f2a7432ca74
dmb-2222 <dmb-2222@yandex.ru>
2019-11-02 13:54:56 +0200

    Updates

---
+++ precache-manifest.619e8c19dbebae009d008457dad9dfe2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66445cb34f58988bf027ea4b12a8541b",
+    "url": "/goit-react14-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "3cab8d6ad9d018f7352d",
+    "url": "/goit-react14-hw-02-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "bb9cf339e4df64a23e8d",
+    "url": "/goit-react14-hw-02-bank-account/static/css/main.d5d95494.chunk.css"
+  },
+  {
+    "revision": "3cab8d6ad9d018f7352d",
+    "url": "/goit-react14-hw-02-bank-account/static/js/2.fd7a6a8c.chunk.js"
+  },
+  {
+    "revision": "bb9cf339e4df64a23e8d",
+    "url": "/goit-react14-hw-02-bank-account/static/js/main.f08ec80c.chunk.js"
+  },
+  {
+    "revision": "ff839732050bfa92fc1f",
+    "url": "/goit-react14-hw-02-bank-account/static/js/runtime-main.bab75a2c.js"
+  }
+]);
