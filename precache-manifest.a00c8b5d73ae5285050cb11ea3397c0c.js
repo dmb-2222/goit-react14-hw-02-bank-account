cb914783f6e0974a03d3388cbf0c234792879d78
dmb-2222 <dmb-2222@yandex.ru>
2019-11-03 12:19:02 +0200

    Updates

---
+++ precache-manifest.a00c8b5d73ae5285050cb11ea3397c0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d3593ca0057241a63fb123de88962c4",
+    "url": "/goit-react14-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "eba5822f3c8ecbc5c793",
+    "url": "/goit-react14-hw-02-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "a9fda77ad197c92a3cb6",
+    "url": "/goit-react14-hw-02-bank-account/static/css/main.7fadf90c.chunk.css"
+  },
+  {
+    "revision": "eba5822f3c8ecbc5c793",
+    "url": "/goit-react14-hw-02-bank-account/static/js/2.11f40224.chunk.js"
+  },
+  {
+    "revision": "a9fda77ad197c92a3cb6",
+    "url": "/goit-react14-hw-02-bank-account/static/js/main.9f886395.chunk.js"
+  },
+  {
+    "revision": "ff839732050bfa92fc1f",
+    "url": "/goit-react14-hw-02-bank-account/static/js/runtime-main.bab75a2c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react14-hw-02-bank-account/precache-manifest.619e8c19dbebae009d008457dad9dfe2.js"
+  "/goit-react14-hw-02-bank-account/precache-manifest.a00c8b5d73ae5285050cb11ea3397c0c.js"
 );
 
 self.addEventListener('message', (event) => {
